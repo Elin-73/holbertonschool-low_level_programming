ea05c63ae559aa191e205cf4554fe967d1e3c09b
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-10-19 23:11:32 +0000

    task 6 1 try

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of variables depending on the bit system
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char myChar;
+	int myInt;
+	long int myLongInt;
+	long long int myLongLontInt;
+	float myFloat;
+
+	printf("Size of a char: ");
+	printf("Size of an int: ");
+	printf("Size of a long int: ");
+	printf("Size of a long long int: ");
+	printf("Size of a float: ");
+	return (0);
+}
