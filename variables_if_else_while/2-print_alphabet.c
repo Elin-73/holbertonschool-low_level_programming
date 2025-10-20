cd1fbb705552b1b76b39a520e1b7ab34303a47ef
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-10-20 01:21:53 +0000

    task 2.2 1 try

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ *
+ * Return: 
+ */
+int main(void)
+{
+	int letter = 0;
+	while (letter != 25)
+	{
+		int currentLetter = 97 + leter;
+		putchar(currentLetter);
+	}
+	putchar('\n');
+}
