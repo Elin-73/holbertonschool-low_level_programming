69e7872b7a9e107a2a02d468d08796b8106bbb43
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-10-20 01:58:14 +0000

    task 2.3 try

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@ int main(void)
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$";
 	int letter = 0;
 
-	while (letter < 53)
+	while (letter < 54)
 	{
 		putchar(alphabet[letter]);
 		letter = letter + 1;
