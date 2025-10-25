c377ce5d92e37fa488d9cd6c862bff11db454887
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-10-25 23:04:33 +0000

    Adding task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * _isupper - checks if the integer inserted is between a range
+ *
+ * Return: gives 1 if it's the range of uppercase letters,
+ * 0 if otherwise
+ */
+int _isupper(int c)
+{
+	if((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
