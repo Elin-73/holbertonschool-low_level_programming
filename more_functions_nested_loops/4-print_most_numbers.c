6fa8d133a0bc0ff400b0457279d0904499b35074
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-10-26 23:25:27 +0000

    adding task 3

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - prints 0-9.
+ *
+ * Return: .
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if ((i == 50) && (i == 52))
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
