34ea20f14a23f3effe59a2eaf7edd5e26fd4b6cd
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-10-26 23:04:27 +0000

    adding task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _putchar - prints 0-9.
+ *
+ * Return: .
+ */
+void _isdigit(void)
+{
+	int i = 48;
+
+	for (i; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(\n);
+}
