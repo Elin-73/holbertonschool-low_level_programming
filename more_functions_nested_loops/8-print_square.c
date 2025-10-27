2cd60bb4d4245b1c429dc6ed9979b08d2459e802
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-10-27 01:16:13 +0000

    adding task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - awaimwdaim
+ *
+ * Return: Always?
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i;
+		int j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
