c63f8c899dc9def2f005e8506b391b5ecaed03dc
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-10-27 02:12:32 +0000

    adding task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - awaimwdaim
+ *
+ * Return: Always?
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i;
+		int j;
+		int k;
+
+		for (i = 0; i < size; i++)
+		{
+			for (k = size-i-1; k > 0; k--)
+			{
+				_putchar(32);
+			}
+			for (j = 0; j <= i; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
