af0ee9715392625df5e22f973c83200bb5b403a6
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-10-27 00:23:55 +0000

    adding task 6

---
+++ more_functions_nested_loops/6_print_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - awaimwdaim
+ *
+ * Return: Always?
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
