1bc0248bf461d6b57b19b6ba411dfb8a94b41946
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-10-27 01:23:04 +0000

    adding task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,14 +8,14 @@
  */
 void print_square(int size)
 {
-	if (n > 0)
+	if (size > 0)
 	{
 		int i;
 		int j;
 
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar(35);
 			}
