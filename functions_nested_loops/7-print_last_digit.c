32f0ff882ad63f0df7aae49eacbef4bbfd9e81cb
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-11-01 18:01:45 +0000

    tasks

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int c)
+{
+	c = c % 10;
+
+	return (c);
+}
