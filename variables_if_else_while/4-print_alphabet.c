f816737d3d88150e22184236ffa3acfcbce8d707
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-11-01 02:32:37 +0000

    task 4 commit

---
+++ variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ *
+ * Return:
+ */
+int main(void)
+{
+	int letter = 0;
+
+	while (letter != 26)
+	{
+		int currentLetter = 97 + letter;
+
+		if ((currentLeter != 101) || (currentLeter != 114°))
+		{
+			putchar(currentLeter);
+		}
+		letter += 1;
+	}
+	putchar('\n');
+	return (0);
+}
