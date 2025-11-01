78d56746bed479516f34bff88b7fd36f0d387063
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-11-01 03:50:50 +0000

    task 7 commit

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ *
+ * Return:
+ */
+int main(void)
+{
+	int letter = 0;
+
+	while (letter != 10)
+	{
+		int currentLetter = 48 + letter;
+
+		putchar(currentLetter);
+		letter = letter + 1;
+	}
+	letter = 0;
+
+	while (letter != 5)
+        {
+                int currentLetter = 97 + letter;
+
+                putchar(currentLetter);
+                letter = letter + 1;
+        }
+	putchar('\n');
+	return (0);
+}
