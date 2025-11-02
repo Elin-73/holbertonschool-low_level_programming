f50e40098c4b16163a044adcb5017a4993643ad0
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-11-02 22:09:52 +0000

    adding task pointers

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - explanation
+ *
+ * return: 0
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *p = dest;
+
+	while (n--)
+	{
+		*p++ = *src++;
+	}
+	return (s);
+}
