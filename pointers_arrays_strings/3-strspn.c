3ee453b65d0da6ab9edbbfa305010d715f8f7102
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-11-02 23:20:55 +0000

    tasks pointers

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - explanation
+ *
+ * return: 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	char *p = s;
+	int fOccur = 1;
+
+	while (*p++)
+	{
+		char *i = accept;
+		int flag = 0;
+
+		while (*i++)
+		{
+			if (*p == *i)
+			{
+				flag = 1;
+			}
+		}
+		if (flag == 1)
+		{
+			return (fOccur);
+		}
+		fOccur += 1;
+	}
+	return (NULL);
+}
