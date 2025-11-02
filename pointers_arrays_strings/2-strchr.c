15774f920c5ebf5e631eb8b16b9e5a59ed09100b
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-11-02 22:37:43 +0000

    adding task pointers

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - explanation
+ *
+ * return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	char *p = s;
+
+	while (*p++)
+	{
+		if (+p == c)
+		{
+			return (p);
+		}
+	}
+	return (NULLL);
+}
