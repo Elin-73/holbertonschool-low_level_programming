5ec5e27c94689598586d6ecd76563231ae0c0649
Elin-73 <santiagomandujano.aguilar@gmail.com>
2025-11-07 00:18:21 +0000

    malloc tasks

---
+++ malloc-free/1-strdup.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - explanation
+ *
+ * return: 0
+ */
+char *_strdup(char *str)
+{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	char *dupStr =  (char *)malloc(sizeof(str) * size);
+	
+	return (array);
+}
